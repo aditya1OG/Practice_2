1a1f469f9ee170de2e41dd11023b4ce273b70404
aditya1OG <136246019+aditya1OG@users.noreply.github.com>
2023-09-23 14:03:10 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
         <div className='flex items-center h-screen justify-around flex-col lg:flex-row border-b-2 border-b-black'>
             <div className='flex flex-col items-center justify-center w-[50%] border-r-2 h-[100vh] border-r-black'>
                 <h1 className='md:text-[35px] text-[25px] font-semibold mb-6 leading-tight text-center'>HELLO IT'S ME <br className='sm:block hidden' /> ADITYA TIWARI APPLIED FOR<br className='sm:block hidden' /><span style={gradientText} className='font-bold text-[30px]'>FRONTEND DEVELOPMENT POSITION</span></h1>
-                <p className='text-[18px] mb-6 w-[70%] text-center'>Welcome to my front-end developer portfolio. I'm a passionate and aspiring front-end developer showcasing my projects and dedication to creating engaging web experiences.</p>
+                <p className='text-[18px] mb-6 w-[70%] text-center'>Hellow Welcome to the Task page, Here I've completed my task successfully for the frontend development position in Educademy LLP. I'm a passionate and aspiring front-end developer showcasing my Skills and dedication to creating engaging web experiences.</p>
                 <button className='flex items-center gap-5 px-4 rounded-3xl py-2 bg-black text-white'>
                     <a href='https://devfolio-6pplfqte8-aditya1q.vercel.app/' className='text-[14px] font-bold'>Visit Portfolio</a>
                 </button>
