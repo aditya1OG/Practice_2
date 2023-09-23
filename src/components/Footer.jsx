0c6882cd8b12556c9665e4c2072f3889ef4d0e74
aditya1OG <136246019+aditya1OG@users.noreply.github.com>
2023-09-23 15:10:39 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col lg:w-1/2 h-[30vh] items-center justify-center border-t-2 border-t-black w-full">
+      <p className="text-black font-semibold text-2xl">Aditya Tiwari</p>
+      <p className="text-sm">Copyright Ⓒ 2023 Aditya Tiwari. All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
