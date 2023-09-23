3bc7db75f236a94420ac2c0e775ca1182934d94a
aditya1OG <136246019+aditya1OG@users.noreply.github.com>
2023-09-23 15:08:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="flex flex-col lg:w-1/2 h-[30vh] items-center justify-center border-t-2 border-t-black w-full">
-      <p className="text-black font-semibold text-2xl">Fresh Bite</p>
-      <p className="text-sm">Copyright Ⓒ 2023 Fresh Bite. All Rights Reserved.</p>
+      <p className="text-black font-semibold text-2xl">Aditya Tiwari</p>
+      <p className="text-sm">Copyright Ⓒ 2023 Aditya Tiwari. All Rights Reserved.</p>
     </div>
   )
 }
