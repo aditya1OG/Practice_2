9cf236d97e9404b8d5f9797266fb095e0c3150e7
aditya1OG <136246019+aditya1OG@users.noreply.github.com>
2023-09-23 15:01:45 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,17 +10,17 @@ const Main = () => {
         display: 'inline-block',
     };
     return (
-        <div className='flex items-center h-[60vh] justify-around flex-col lg:flex-row border-b-2 border-b-black'>
-            <div className='flex flex-col items-center justify-center w-[50%] border-r-2 h-[60vh] border-r-black'>
+        <div className='flex items-center h-screen mt-24 lg:mt-0 lg:h-[60vh] justify-between flex-col lg:flex-row lg:border-b-2 lg:border-b-black w-full'>
+            <div className='flex flex-col gap-5 items-center justify-center lg:w-[50%] lg:border-r-2 h-[60vh] lg:border-r-black w-full mb-10 lg:mb-0'>
                 <h1 className='md:text-[25px] text-[20px] font-semibold mb-6 leading-tight text-center'>HELLO IT'S ME <br className='sm:block hidden' /> ADITYA TIWARI APPLIED FOR<br className='sm:block hidden' /><span style={gradientText} className='font-bold text-[25px]'>FRONTEND DEVELOPMENT POSITION</span></h1>
-                <p className='text-[14px] mb-6 w-[70%] text-center'>Hellow Welcome to the Task page, Here I've completed my task successfully for the frontend development position in Educademy LLP. I'm a passionate and aspiring front-end developer showcasing my Skills and dedication to creating engaging web experiences</p>
-                <button className='flex items-center gap-5 px-4 rounded-3xl py-2 bg-black text-white'>
+                <p className='text-[14px] mb-6 w-[65%] text-center'>Hellow Welcome to the Task page, Here I've completed my task successfully for the frontend development position in Educademy LLP. I'm a passionate and aspiring front-end developer showcasing my Skills and dedication to creating engaging web experiences</p>
+                <button className='flex items-center gap-5 px-6 rounded-3xl py-2 bg-black text-white'>
                     <a href='https://devfolio-6pplfqte8-aditya1q.vercel.app/' className='text-[14px] font-bold'>Visit Portfolio</a>
                 </button>
             </div>
 
-            <div className='flex items-center justify-center'>
-                <img src={pgImage} alt="" className='image object-contain flex items-center justify-center w-[70%] h-[60vh]' />
+            <div className='flex items-center justify-center mt-5 lg:mt-0'>
+                <img src={pgImage} alt="" className='object-contain flex items-center justify-center w-[70%] h-[60vh] mr-0 lg:mr-32' />
             </div>
         </div>
     )
